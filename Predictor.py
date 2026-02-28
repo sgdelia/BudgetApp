99ed14b5f6e98a327509fe9d8560183903b8e5f2
Sophia Delia <sgdelia31@gmail.com>
2026-02-28 09:01:56 -0500

    Configuration, classification

--- Predictor.py
+++ Predictor.py
@@ -3,6 +3,7 @@ import numpy as np
 import sklearn as sk
 import sys
 import pathlib
+import argparse
 
 def importAndClean(fileName):
     properFormat = fileName.replace('"','').replace('"', '')
@@ -19,11 +20,50 @@ def importAndClean(fileName):
         workingData[header] = extractedCol
     return workingData
 
+def ConvertToValues(workingData):
+    # convert description to values
+    # convert date to values
+    df = workingData.copy()
+    AmountClassifiers = pd.Series(["Very Low", "Low", "Medium", "High", "Very High", None])
+    thresholds = pd.Series([0, 20, 50, 100, 500, np.inf])
+    df['Amount'] = np.where(df['Amount'] <= thresholds[1], AmountClassifiers[0],
+                    np.where(df['Amount'] <= thresholds[2], AmountClassifiers[1],
+                    np.where(df['Amount'] <= thresholds[3], AmountClassifiers[2],
+                    np.where(df['Amount'] <= thresholds[4], AmountClassifiers[3],
+                    np.where(df['Amount'] <= thresholds[5], AmountClassifiers[4], AmountClassifiers[5])))))
+   
+    # convert categories to values
+    CategoryClassifiers = df['Category'].unique()
+    CategoryClassifiers = np.append(CategoryClassifiers, None)
+    CategoryClassifiers = pd.Series(CategoryClassifiers)
+    # convert description to values
+    DescriptionClassifiers = df['Description'].unique()
+    DescriptionClassifiers = np.append(DescriptionClassifiers, None)
+    DescriptionClassifiers = pd.Series(DescriptionClassifiers)
+    return df
+    
+    
+
+def initializeModel(workingData):
+    # initialize model here
+    X = workingData[["Amount", "Description"]]
+    y = workingData["Category"]
+    r = sk.linear_model.LinearRegression()
+    r.fit(X, y)
+    return r
+
 def main():
     print("This is the predictor file. It will be used to categorize expenses based on location and amount spent.")
-    fileName = input("Enter the name of the file you want to use: ")
+    parser = argparse.ArgumentParser(description='Process the budget file.')
+    parser.add_argument('--filepath', type=str, help='the path to the budget file')
+    args = parser.parse_args()
+    fileName = args.filepath
     workingData = importAndClean(fileName)
-    print(workingData)
+    processedData =ConvertToValues(workingData)
+    print(processedData.head())
+
+    # model = initializeModel(workingData)
+    # print(model)
 
 if __name__=="__main__":
     main()
