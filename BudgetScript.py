27b7e5036ec23e8aa97eea0fd2bc681566953405
Sophia <sgdelia31@gmail.com>
2026-03-24 22:42:12 -0400

    Added args

--- BudgetScript.py
+++ BudgetScript.py
@@ -1,20 +1,36 @@
 import pandas as pd
 import numpy as np
 import sys
-from PySide6.QtWidgets import QApplication, QLabel
+import argparse
+
 from pathlib import Path
 
-def importAndClean(fileName):
+def importAndClean(fileName, typeOfStatment: str = 'other'):
     properFormat = fileName.replace('"','').replace('"', '')
     properFormat = Path(properFormat)
-    cleaned = pd.read_excel(properFormat, sheet_name=0, header=0)
-    print(cleaned.head())   
+
+    if typeOfStatment.upper() == 'AMEX':
+        cleaned = pd.read_excel(properFormat, sheet_name=0, header=6)
+    elif typeOfStatment.upper() == 'BANK':
+        cleaned = pd.read_csv(properFormat)
+        colsToDrop = ['Bank RTN', 'Account Number', 'Transaction Type', 'Check Number',]
+        cleaned = cleaned.drop(colsToDrop, axis=1)
+        CredsAndDebs = cleaned['Debit'].to_frame()
+        CredsAndDebs = CredsAndDebs.join(cleaned['Credit'].to_frame())
+        Total = CredsAndDebs['Debit'].sum() - CredsAndDebs['Credit'].sum()
+        cleaned.insert(1,'Amount', Total)
+        try:
+            index: int = cleaned.columns.names.count('Category')
+        except:
+            cleaned.insert(3,column='Category', value="")
+        cleaned = cleaned.drop(['Credit', 'Debit'], axis=1)
+    else:
+        cleaned = pd.read_excel(properFormat, sheet_name=0, header=0)
     #get Date, Amount, Description, and "In Spreadsheet" columns
 
     # Prompt for option to include "Category"?
     # headers = ["Date", "Amount", "Description", "In Spreadsheet", "Category"]
     headers = ["Date", "Amount", "Description", "Category"]
-
     workingData = pd.DataFrame({})
     for header in headers:
         extractedCol = cleaned[header]
@@ -41,15 +57,19 @@ def excelFormatting(workingData):
         formula = formula.removesuffix("+") 
         dictOfVals = dict(Formula=formula, Category=group[0])  
         formsAndGroups.append(dictOfVals)
-    pd.DataFrame(formsAndGroups).to_clipboard()
+    framed = pd.DataFrame(formsAndGroups)
+    framed.to_clipboard(index=False, header=False)
     print("Excel formulas copied to clipboard")
     
 
-
-
 def main():
-    filepath = input("Enter the path to the budget file: ")
-    workingData = importAndClean(filepath)
+    parser = argparse.ArgumentParser(prog='Budget Script', description='Parses Excel spreadsheets of spending data, consolidates, and creates budget formulas')
+    parser.add_argument('-p', '--path', type=str, help='Path to expense export (excel files only)', required=True)
+    parser.add_argument('-t', '--type', type=str, required=True, help="Type of statement: amex, bank, or other")
+    args = parser.parse_args()
+    filepath = args.path
+    typeOfStatement: str = args.type
+    workingData = importAndClean(filepath, typeOfStatement)
     print(workingData)
     categorize(workingData)
     excelFormatting(workingData)
