610c2dc4fb767d6d91f134ef7578b455753dd15a
Sophia <sgdelia31@gmail.com>
2026-03-27 16:55:53 -0400

    trimming fat so I can start over

--- Predictor.py
+++ Predictor.py
@@ -29,47 +29,38 @@ def ConvertToValues(workingData):
     # convert date to values
     df = workingData.copy()
     AmountClassifiers = pd.Series(["Very Low", "Low", "Medium", "High", "Very High", None])
-    thresholds = pd.Series([0, 20, 50, 100, 500, int(10**6)])
-   
     # convert categories to values
-    CategoryClassifiers = df['Category'].unique()
-    CategoryClassifiers = np.append(CategoryClassifiers, None)
-    CategoryClassifiers = pd.Series(CategoryClassifiers)
+    categories: list[str] = []
+    for cat in df['Category']:
+        category = str.upper(cat).strip()
+        categories.append(category)
+        df = df.replace(cat,category)
+    np.append(categories, None)
+    CategoryClassifiers = pd.Series(categories).unique()
     # convert description to values
     DescriptionClassifiers = df['Description'].unique()
     DescriptionClassifiers = np.append(DescriptionClassifiers, None)
-    DescriptionClassifiers = pd.Series(DescriptionClassifiers)
     
-    df['Amount'] = pd.cut(df['Amount'], bins=thresholds, labels=AmountClassifiers[:-1], right=False)
-    df['Category'] = pd.Categorical(df['Category'], categories=CategoryClassifiers[:-1])
-    df['Description'] = pd.Categorical(df['Description'], categories=DescriptionClassifiers[:-1])
-    return df, thresholds, AmountClassifiers, CategoryClassifiers, DescriptionClassifiers
+    df['Amount'] = pd.cut(df['Amount'], bins=CategoryClassifiers.size)
     
-def predict(args=None, filePath=None):
+    return df, AmountClassifiers, CategoryClassifiers, DescriptionClassifiers
+    
+def predict(filePath=None):
     # predict category based on amount and description
     # print("This is the predictor file. It will be used to categorize expenses based on location and amount spent.")
-    if args is None and filePath is not None:
-        args = argparse.Namespace(filepath=filePath)
-    else:
-        parser = argparse.ArgumentParser(description='Process the budget file.')
-        parser.add_argument('--filepath', type=str, help='the path to the budget file', default=filePath)
-        args = parser.parse_args()
-    try:
-        if args.filepath is None:
-            raise ValueError("File path is required. Please provide a valid file path using the --filepath argument.")
-        fileName = args.filepath
-        workingData = importAndClean(fileName)
-        return workingData
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    if filePath == None:
+        print("File path required. Exiting.")
+        quit
+    workingData = importAndClean(filePath)
+    outData = ConvertToValues(workingData=workingData)
+    return outData
    
     
-
 def main():
-    predict(args=sys.argv[2:])
+    parser = argparse.ArgumentParser(description='Process the budget file.')
+    parser.add_argument('--filepath', type=str, help='the path to the budget file', required=True)
+    args = parser.parse_args()
+    predict(args.filepath)
 
 if __name__=="__main__":
-    PredictorPath = str(pathlib.Path().resolve().parents[0] / "src")
-    if PredictorPath not in sys.path:
-        sys.path.insert(0, PredictorPath)
     main()
