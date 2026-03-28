1c8f0c436d0608f6f23553b8f05b47a9dcf91f6d
Sophia <sgdelia31@gmail.com>
2026-03-28 02:10:16 -0400

    lord help me it is 2:10 AM EST

--- Predictor.py
+++ Predictor.py
@@ -1,13 +1,6 @@
-import matplotlib
-import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
-import sklearn as sk
-from sklearn.linear_model import LinearRegression
-import sys
 import pathlib
 import argparse
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QFileDialog
 
 def importAndClean(fileName):
     properFormat = fileName.replace('"','').replace('"', '')
@@ -15,44 +8,47 @@ def importAndClean(fileName):
     cleaned = pd.read_excel(properFormat, sheet_name=0, header=0, engine='openpyxl')
     # print(cleaned.head())   
     #get Date, Amount, Description, and "Category" columns
-
     headers = ["Date", "Amount", "Description", "Category"]
-
     workingData = pd.DataFrame({})
     for header in headers:
         extractedCol = cleaned[header]
         workingData[header] = extractedCol
     return workingData
 
-def ConvertToValues(workingData):
+def CategoryCleaning(workingData):
     # convert description to values
     # convert date to values
-    df = workingData.copy()
-    AmountClassifiers = pd.Series(["Very Low", "Low", "Medium", "High", "Very High", None])
+    df: pd.DataFrame = workingData.copy()
     # convert categories to values
     categories: list[str] = []
     for cat in df['Category']:
-        category = str.upper(cat).strip()
+        category = str.upper(cat).strip()[:5]
         categories.append(category)
-        df = df.replace(cat,category)
-    np.append(categories, None)
-    CategoryClassifiers = pd.Series(categories).unique()
-    # convert description to values
-    DescriptionClassifiers = df['Description'].unique()
-    DescriptionClassifiers = np.append(DescriptionClassifiers, None)
+    df['Category'] = pd.DataFrame(categories)
+    return df
+
+def DescCleaning(workingData):
+    df:pd.DataFrame = workingData.copy()
+    descriptions: list[str] = []
+    for desc in df['Description']:
+        descrip = str.upper(desc).strip()[:10]
+        descriptions.append(descrip)
+    df['Description'] = pd.DataFrame(descriptions)
+    return df
     
-    df['Amount'] = pd.cut(df['Amount'], bins=CategoryClassifiers.size)
     
-    return df, AmountClassifiers, CategoryClassifiers, DescriptionClassifiers
-    
-def predict(filePath=None):
+def Clean(filePath=None) -> pd.DataFrame:
     # predict category based on amount and description
     # print("This is the predictor file. It will be used to categorize expenses based on location and amount spent.")
     if filePath == None:
         print("File path required. Exiting.")
         quit
     workingData = importAndClean(filePath)
-    outData = ConvertToValues(workingData=workingData)
+    cleanCats: pd.DataFrame = CategoryCleaning(workingData=workingData)
+    cleanDescs: pd.DataFrame = DescCleaning(workingData=workingData)
+    outData: pd.DataFrame = workingData.copy()
+    outData['Description'] = cleanDescs['Description']
+    outData['Category'] = cleanCats['Category']
     return outData
    
     
@@ -60,7 +56,9 @@ def main():
     parser = argparse.ArgumentParser(description='Process the budget file.')
     parser.add_argument('--filepath', type=str, help='the path to the budget file', required=True)
     args = parser.parse_args()
-    predict(args.filepath)
+    Clean(args.filepath)
 
 if __name__=="__main__":
     main()
+    
+    
